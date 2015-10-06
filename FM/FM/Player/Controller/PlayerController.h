ef202b65a6cb935ceca11be215c664fbb4465c00
yy <lanou1506@163.com>
2015-10-06 19:55:20 +0800

    [add]添加多个文件

---
+++ FM/FM/Player/Controller/PlayerController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerController.h
+//  FM
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 YT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerController : UIViewController
+
+@end
---
+++ FM/FM/Player/Controller/PlayerController.m
@@ -0,0 +1,37 @@
+//
+//  PlayerController.m
+//  FM
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 YT. All rights reserved.
+//
+
+#import "PlayerController.h"
+
+@interface PlayerController ()
+
+@end
+
+@implementation PlayerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
