e94cb77800f2a642ec7bef0bc88256cd19933e5d
lanou3g <136925565@qq.com>
2015-10-06 15:39:44 +0800

    我改了个亮亮

--- FM/FM/AppDelegate.m
+++ FM/FM/AppDelegate.m
@@ -17,7 +17,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    
+    
+    NSLog(@"亮亮个逗比");
+    
+    
+    
+    
+    
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
